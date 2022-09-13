88f0b8dfd747d5a11d2d2cb6b543fc73f928a539
Thomas Vermette <68394222+masver-code@users.noreply.github.com>
2022-09-13 14:02:21 -0400

    Add files via upload

---
+++ Jokes/script.js
@@ -0,0 +1,39 @@
+const jokeElement = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click',generateJoke)
+generateJoke
+
+//async
+async function generateJoke(){
+  const config={ 
+     headers:{
+    'Accept': 'application/json'
+  }
+}
+   const res = await fetch('https://icanhazdadjoke.com/', config)
+ 
+  const data = await res.json()
+
+  jokeElement.innerHTML =data.joke
+  
+  
+}
+
+//non async
+
+
+// function generateJoke(){
+//   const config={ 
+//      headers:{
+//     'Accept': 'application/json'
+//   }
+// }
+//   fetch('https://icanhazdadjoke.com/', config)
+//   .then(res => res.json())
+//   .then((data) => {
+//     jokeElement.innerHTML =data.joke
+//   })
+  
+  
+// }
